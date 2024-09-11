a7b032c9d0f3924a25e5c982a99b1c3ad1612c43
SonDangJH <son.dang@journeyh.io>
2024-09-11 16:00:47 +0700

    fix ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,25 +1,31 @@
 
 //Ex 2: Given an array of integers, find integers with the most repetitions. If multiple numbers have the same maximum number of repetition, export all of them.
 //Maximum 3 rounds, not nested.
-const arr = [2, 4, 4, 5, 3, 4, 1, 7]; 
-const n = arr.length; 
-const k = 8; 
+// const arr = [1, 2, 2, 3, 3, 3, 4, 4, 4];
+const arr = [1, 2, 2, 3, 3, 3, 4, 4, 4, 5,5,5,5,5];
 
-  function maxRepeating(arr, n, k) { 
-    for (let i = 0; i< n; i++) 
-        arr[arr[i]%k] += k; 
- 
-    let max = arr[0], result = 0; 
-    for (let i = 1; i < n; i++) 
-    { 
-        if (arr[i] > max) 
-        { 
-            max = arr[i]; 
-            result = i; 
-        } 
-    } 
- 
-    return result; 
-} 
+function findMostRepeated(arr) {
+    const countMap = {};
+    let maxReps = 0;
+    const result = [];
+  
+   
+    for (let i = 0; i < arr.length; i++) {
+      const num = arr[i];
+      countMap[num] = (countMap[num] || 0) + 1;
+      maxReps = Math.max(maxReps, countMap[num]);
+    }
+  
+    for (let num in countMap) {
+      if (countMap[num] === maxReps) {
+        result.push(Number(num));
+      }
+    }
+  
+    return result;
+  }
+  
 
-console.log("The maximum repeating number is " + maxRepeating(arr, n, k))
+  const result = findMostRepeated(arr);
+  console.log(result);
+  
